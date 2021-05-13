c4475d6a654b4b20cd5a377e13bcb9111aab537e
leon-i <isakleon97@gmail.com>
2021-05-13 14:33:09 -0400

    Added comment

--- src/card_data.ts
+++ src/card_data.ts
@@ -12,17 +12,20 @@ import { Card } from './types'
  }
 */
 export const getCardData = async (): Promise<void> => {
-  let allCardData : Card[] = []
-  const setNumbers : number[] = [1, 2, 3, 4]
+  let allCardData: Card[] = []
+  const setNumbers: number[] = [1, 2, 3, 4]
 
   // collect card data from each set
   for (const setNumber of setNumbers) {
-    const data = await fetch(`https://dd.b.pvp.net/latest/set${setNumber}/en_us/data/set${setNumber}-en_us.json`)
-    const cardData : Card[] = await data.json()
+    const data = await fetch(
+      `https://dd.b.pvp.net/latest/set${setNumber}/en_us/data/set${setNumber}-en_us.json`,
+    )
+    const cardData: Card[] = await data.json()
 
     allCardData = [...allCardData, ...cardData]
   }
 
+  // get only champions from card data
   const allChamps = allCardData.filter((card) => card.rarityRef === 'Champion')
   allChamps.forEach((champ) => (CHAMP_DATA[champ.cardCode] = champ.name))
 }
